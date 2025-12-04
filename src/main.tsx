61097af0cf117e7600abbfea0e45093c01bf9b2d
shahidsaaqib <pitacoin8@gmail.com>
2025-12-04 13:42:13 +0000

    Fix: Add SPA routing support for GitHub Pages with 404.html redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+// Handle redirect from 404 page for single-page apps
+if (window.location.pathname.includes('/?/')) {
+	const newPath = '/' + window.location.pathname.split('/?/')[1].replace(/~and~/g, '&');
+	window.history.replaceState(null, '', newPath);
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
